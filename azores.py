d7684eabfd25fc78067efd46b3f18db6dc64bdcf
KamishiroRuiLover <iplayfo4@gmail.com>
2024-12-20 21:53:15 -0600

    add to template game, switch csv to json, update azores

--- azores.py
+++ azores.py
@@ -1,23 +1,35 @@
 import pygame as pg
+import sys
+
+
+if len(sys.argv) != 2:
+    print("USAGE: python azores.py games/FOLDER")
+    sys.exit()
 
 
 # Initializing the pygame screen
 screen = None
 clock = None
 running = True
+
 def initializePygame():
     pg.init()
     screen = pg.display.set_mode((1280, 720))
     clock = pg.time.Clock()
 initializePygame()
 
+def read_core(path):
+    path += "/core.txt" # makes sure we're reading core
+    f = open(path, "r")
+    title = f.readline()
+    pg.display.set_caption(title) # sets title of page to game title.
+read_core(sys.argv[1])
 
-def main():
-    while running:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
+
+while running:
+    for event in pg.event.get(): # closes game when player clicks X button
+        if event.type == pg.QUIT:
+            running = False
 
 
-main()
 pg.quit()
