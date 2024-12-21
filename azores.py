5557bd4b4fe786622ac5a35c234da031a5b7b145
KamishiroRuiLover <iplayfo4@gmail.com>
2024-12-20 20:54:30 -0600

    update Azores

--- azores.py
+++ azores.py
@@ -3,6 +3,20 @@ import pygame as pg
 
 # Initializing the pygame screen
 screen = None
+clock = None
 running = True
 def initializePygame():
     pg.init()
+    screen = pg.display.set_mode((1280, 720))
+    clock = pg.time.Clock()
+initializePygame()
+
+def main():
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+
+main()
+pg.quit()
